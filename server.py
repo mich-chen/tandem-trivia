7f8d221ac648bf5ed1aff0a7657a1d32dcb880ac
Michelle Chen <mich.chen.94@gmail.com>
2020-10-26 16:35:33 -0700

    catch all route

--- server.py
+++ server.py
@@ -9,3 +9,11 @@ app = Flask(__name__, static_url_path='/',
                   static_folder='build',
                   template_folder='build')
 
+# catch all
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    """Catch all URL routes that don't match specific path."""
+
+    return render_template('root.html')
+
