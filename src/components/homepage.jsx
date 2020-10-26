5821d314eca60f4bea8bb1129876e1a1e67b7751
Michelle Chen <mich.chen.94@gmail.com>
2020-10-26 16:19:42 -0700

    test header and event handler button

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 function Homepage() {
-    return (
-        <h1>Welcome to Tandem Trivia For 400!</h1>
-        );
+  const handleClick = () => {
+    alert('hello, button successfully clicked!');
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Tandem Trivia For 400!</h1>
+      <Button onClick={handleClick}> Start Trivia! 
+      </Button>
+    </div>
+      );
 }
 
 export default Homepage;
