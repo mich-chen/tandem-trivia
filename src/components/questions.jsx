499f66664854f687aa0d3ef6ca149b5c361ff98b
Michelle Chen <mich.chen.94@gmail.com>
2020-10-27 02:26:19 -0700

    display question and multiple choices, logic to show correct answer

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,31 +1,70 @@
 import React from "react";
+// import Button from 'react-bootstrap/Button';
 // import jsonData from '../Apprentice_TandemFor400_Data.json';
 
-// function Question() {
-//   return (
-//     );
-// }
+function Question(props) {
+  const { question } = props.current;
+  return (
+    <div>
+      {question}
+    </div>
+    );
+}
 
-// function Choice() {
 
-// }
+function MultipleChoices(props) {
+  const { choices, correct } = props.current;
+  // console.log(typeof(correct));
 
-// function MultipleChoices() {
 
-// }
+  const handleClick = (e, ans) => {
+    console.log((ans));
+    // correct answer turns green (change color)
+    if (ans === correct) {
+      alert('correct answer!');
+       e.target.style.color = 'green'
+    } else {
+      alert('wrong answer!')
+    };
+  };
+
+  return (
+    <ul>
+      {choices.map((answer) => 
+        <div key={choices.indexOf(answer)}
+             value={answer}
+             onClick={(e) => {handleClick(e, answer)}}>
+        {answer}
+        </div>)
+      }
+    </ul>
+    );
+}
+
 
 function QuestionsContainer(props) {
   // const data = JSON.parse(JSON.stringify(jsonData));
   const { questions } = props;
-  console.log(questions);
+  const [currentQ, setCurrentQ] = React.useState({});
+  const [asked, setAsked] = React.useState([]);
+  console.log('current', currentQ);
+  console.log('asked', asked);
 
-  // logic to randomly select a question using index
-  let current = questions[Math.floor(Math.random() * questions.length)];
-  console.log(current);
+  // 10 questions for each round of trivia
+  for (let i = 0; i < 10; i++) {
+    // logic to randomly select a question using index
+    let random = questions[Math.floor(Math.random() * questions.length)];
+    if (!asked.includes(random)) {
+        setCurrentQ(random);
+        let new_asked = asked;
+        setAsked(new_asked.concat(random))
+    };
+  };
 
   return (
     <div>
-      hello
+      <Question current={currentQ} />
+      <MultipleChoices current={currentQ} />
     </div>
       );
 }
