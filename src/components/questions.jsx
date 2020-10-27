dc6d363a91530113b807d2fd859d9cf7e135d66a
Michelle Chen <mich.chen.94@gmail.com>
2020-10-27 01:00:40 -0700

    randomizing questions logic

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,18 +1,6 @@
 import React from "react";
 // import jsonData from '../Apprentice_TandemFor400_Data.json';
 
-function QuestionsContainer(props) {
-  // const data = JSON.parse(JSON.stringify(jsonData));
-  console.log(props);
-
-
-  return (
-    <div>
-      hello
-    </div>
-      );
-}
-
 // function Question() {
 //   return (
 //     );
@@ -25,5 +13,21 @@ function QuestionsContainer(props) {
 // function MultipleChoices() {
 
 // }
+
+function QuestionsContainer(props) {
+  // const data = JSON.parse(JSON.stringify(jsonData));
+  const { questions } = props;
+  console.log(questions);
+
+  // logic to randomly select a question using index
+  let current = questions[Math.floor(Math.random() * questions.length)];
+  console.log(current);
+
+  return (
+    <div>
+      hello
+    </div>
+      );
+}
 
 export default QuestionsContainer;
