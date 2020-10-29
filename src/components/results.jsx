ea4bed09768504ff4b5532853d3158d52efa6278
Michelle Chen <mich.chen.94@gmail.com>
2020-10-28 18:56:08 -0700

    button to restart game

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 function Results(props) {
+  const { results, setStartGame, setEndGame } = props;
+  const handleClick = () => {
+    setEndGame(false);
+    setStartGame(true)
+  }
+
   return (
     <div>
       <h2> Congratulations! </h2>
-      <h3> You got {props.results} out of 10 correct! </h3>
+      <h3> You got {results} out of 10 correct! </h3>
+
+      <br />
+
+      <Button onClick={handleClick}>
+        Click here to play again!
+      </Button>
     </div>
   );
 }
