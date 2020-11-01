6f8764d6bf0acb9799dc29adac782dc5e8436825
Michelle Chen <mich.chen.94@gmail.com>
2020-11-01 15:30:19 -0800

    test useEffect updated shuffle state

--- src/components/multipleChoice.test.js
+++ src/components/multipleChoice.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import MultipleChoices from './MultipleChoice';
 
 test('4 answer choices on screen', () => {
@@ -11,15 +12,16 @@ test('4 answer choices on screen', () => {
   expect(screen.getByText('4')).toBeInTheDocument();
 });
 
-// test('shuffled answer choices', () => {
-//   const currentQ = {'choices': [1, 2, 3, 4], 'correct': 1};
-//   render(<MultipleChoices current={currentQ} />);
-//   expect.hasAssertions();
-
-// })
-
 test('useEffect updated shuffle state', () => {
   const currentQ = {'choices': [1, 2, 3, 4], 'correct': 1};
   const result = render(<MultipleChoices current={currentQ} />);
   expect(result.state).not.toEqual([]);
 });
+
+// test('update selected on click', () => {
+//   const spy = jest.spyOn(MultipleChoices, 'getColor');
+//   const currentQ = {'choices': [1, 2, 3, 4], 'correct': 1};
+//   render(<MultipleChoices current={currentQ} />)
+//   fireEvent.change(screen.getByTestId("3"));
+//   expect(spy).toHaveBeenCalled();
+// });
