c25e7f71c56bc4ecad17f7301c81cad0e93a3ebf
Michelle Chen <mich.chen.94@gmail.com>
2020-11-01 12:26:42 -0800

    add test to see welcome message

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,4 +1,4 @@
-// import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -7,4 +7,9 @@ import App from './App';
 test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+test('renders welcome message', () => {
+  render(<App />);
+  expect(screen.getByText('Welcome to Tandem Trivia For 400!')).toBeInTheDocument();  
 });
