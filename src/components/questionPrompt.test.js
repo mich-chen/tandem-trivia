1a7d4dc947d2326571bd502d2868ef31dd88bc69
Michelle Chen <mich.chen.94@gmail.com>
2020-11-01 15:41:01 -0800

    test showing number and question

---
+++ src/components/questionPrompt.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Question from './questionPrompt';
+
+test('show number and question', () => {
+  const q = {'question': 'test question'};
+  const container = render(<Question current={q}
+                   number={1} />);
+  expect(screen.getByTestId('question')).toHaveTextContent('1. test question');
+});
