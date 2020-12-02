3044e90ff6d0df60524e8df63fbbb78646d38596
Michelle Chen <mich.chen.94@gmail.com>
2020-12-01 18:13:11 -0800

    set up TestFlask class, setup method, and test setup

--- tests/pytest_test.py
+++ tests/pytest_test.py
@@ -10,7 +10,7 @@ import helper
 @pytest.fixture(autouse=True)
 def file_path():
     server.FILE = 'tests/test_data.json'
-    return server.FILE
+    yield server.FILE
 
 @patch('server.FILE', 'tests/test_data.json')
 def test_file_path():
@@ -18,3 +18,15 @@ def test_file_path():
 
 def test_file_path_fixture(file_path):
     assert server.FILE == 'tests/test_data.json'
+
+class TestFlask:
+
+    # setup Flask test client and file path
+    def setup_method(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+        self.data = helper.open_file('test_data.json')
+
+
+    def test_setup(self):
+        assert self.data != None
